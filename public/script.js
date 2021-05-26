09ba75c1b3d8771e02345d047f3eb1beab9f717f
Le Berre <jeremy.leberre2021@campus-eni.fr>
2021-05-26 10:14:02 +0200

    test

---
+++ public/script.js
@@ -0,0 +1,11 @@
+let tableau =[];
+
+function ajouter(){
+
+    let nom = document.getElementById('nom').value;
+    let prenom = document.getElementById('prenom').value;
+
+    document.getElementById('nom').value = '';
+    document.getElementById('prenom').value = '';
+    
+}
